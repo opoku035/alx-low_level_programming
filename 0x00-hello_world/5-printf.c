52fb88c697b95f90d8ec58ff97dddb4d3ff2b27b
Alexander Opoku <opokualex556@gmail.com>
2022-07-01 14:18:39 -0700

    sizes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h
+#include <stdio.h>
 /**
  * main - contains my program
- * 
  *
- * return: Always 0 (success)
+ *
+ * Return: Always 0 for success
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
