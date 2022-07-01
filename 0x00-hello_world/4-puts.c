4aae3507f4b859dc30534cfed82229739939520a
Alexander Opoku <opokualex556@gmail.com>
2022-06-30 18:42:02 -0700

    multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+main - Contains my program
+ 
+
+Return: Always 0 (Success)
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-}		
+
+}
