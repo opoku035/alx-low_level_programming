21a323d7ad5688b3564fc2b700d48cd5eb8aaf62
opoku035 <opokualex556@gmail.com>
2022-07-05 21:59:04 +0000

    tsk 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/**
+*
+* Description: (int c) is used to check results
+*
+* Return: int 0
+*/
+int _islower(int c)
+{
+char check;
+
+check = tolower(c);
+if (c != check || c == 0 || c == 20)
+{
+return (0);
+}
+if (c == check)
+{
+return (1);
+}
+return (0);
+}
